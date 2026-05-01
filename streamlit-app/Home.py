d4c310d469d237556074bbf5a36bf73f19c0db9e
DWandless <drew.wandless@dxc.com>
2026-05-01 09:37:13 +0100

    Update page title and remove outdated README

--- streamlit-app/Home.py
+++ streamlit-app/Home.py
@@ -11,7 +11,7 @@ from streamlit.components.v1 import html as st_html
 
 # ------------------ PAGE CONFIG ------------------
 logo_path2 = Path(__file__).resolve().parent / "assets" / "logo3.png"
-st.set_page_config(page_title="🏃 Movember Step Tracker", layout="wide", page_icon=logo_path2)
+st.set_page_config(page_title="DXC Step Tracker", layout="wide", page_icon=logo_path2)
 
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
