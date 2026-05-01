951a6b42613006ce9ec749d13533d71b8449948f
DWandless <drew.wandless@dxc.com>
2026-05-01 09:36:50 +0100

    Remove unused Procfile, assets, and utility modules

--- streamlit-app/src/data/__init__.py
+++
@@ -1,2 +0,0 @@
-# filepath: streamlit-app/src/data/__init__.py
-# This file is intentionally left blank.
--- streamlit-app/src/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# filepath: streamlit-app/src/utils/__init__.py
-# This file marks the utils directory as a package. It can be used to import utility functions from this directory.
